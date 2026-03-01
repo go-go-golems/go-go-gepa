9516de08709006c67b299a0ea2696370750d6ffc
Manuel Odendahl <wesen@ruinwesen.com>
2026-03-01 07:41:41 -0500

    docs(ttmp): move cross-repo GEPA tickets to owning repositories

--- ttmp/2026/02/27/GEPA-12-ARC-AGI-OS-BACKEND-MODULE--arc-agi-backend-module-integration-for-go-go-os-and-wesen-os/scripts/run_arc_server_offline.py
+++
@@ -1,8 +0,0 @@
-import arc_agi
-from arc_agi import OperationMode
-
-arc = arc_agi.Arcade(
-    operation_mode=OperationMode.OFFLINE,
-    environments_dir="test_environment_files",
-)
-arc.listen_and_serve(host="0.0.0.0", port=18081)
